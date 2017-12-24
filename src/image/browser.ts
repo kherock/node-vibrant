be9787c48752b193b54b443ce266b635e4a0708d
Kyle Herock <kherock@gmail.com>
2017-12-24 09:09:25 -0500

    Append the canvas element before attempting to do drawImage()
    This avoid errors cleaning up the canvas element if an error occurs while drawing the image

--- src/image/browser.ts
+++ src/image/browser.ts
@@ -33,13 +33,12 @@ export default class BroswerImage extends ImageBase {
 
         canvas.className = 'vibrant-canvas'
         canvas.style.display = 'none' 
+        document.body.appendChild(canvas)
 
         this._width = canvas.width = img.width
         this._height = canvas.height = img.height
 
         context.drawImage(img, 0, 0)
-
-        document.body.appendChild(canvas)
     }
     load(image: ImageSource): Bluebird<ImageBase> {
         let img: HTMLImageElement = null
